83f229a78d0089ba2c61634b643e529d23dece3e
Luan Brandão <114003879+luan-brandao@users.noreply.github.com>
2024-10-10 18:54:05 -0300

    Update RoomRepository.js

--- api/src/repositories/RoomRepository.js
+++ api/src/repositories/RoomRepository.js
@@ -2,7 +2,7 @@
 const Room = require('../models/RoomModel');
 
 class RoomRepository {
-    // Método para criar uma nova sala
+    
     static async createRoom(roomData) {
         const room = new Room({
             ...roomData,
@@ -12,12 +12,12 @@ class RoomRepository {
         return room;
     }
 
-    // Método para obter todas as salas
+    
     static async getAllRooms() {
         return await Room.find();
     }
 
-    // Método para entrar na sala
+    
     static async enterRoom(roomId) {
         const room = await Room.findById(roomId);
         
@@ -25,7 +25,7 @@ class RoomRepository {
             throw new Error('Sala não encontrada');
         }
 
-        // Verifica se a capacidade máxima foi atingida
+        
         if (room.currentParticipants < room.capacity) {
             room.currentParticipants += 1; 
             return room;
@@ -34,7 +34,7 @@ class RoomRepository {
         }
     }
 
-    // Método para sair da sala
+    
     static async leaveRoom(roomId) {
         const room = await Room.findById(roomId);
         
